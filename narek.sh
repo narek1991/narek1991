497297f24033a22ff0df638265a14e5f3c6eefb9
narek1991 <31657667+narek1991@users.noreply.github.com>
2021-10-11 20:24:52 +0400

    Update narek.sh

--- narek.sh
+++ narek.sh
@@ -9,15 +9,22 @@ echo $forscr
 forscr1=555555
 echo $forscr1
 
+#var 1-3
+forscr3="$555555"
+echo $forscr3
+
+
 #arguments
 echo $1
 echo $2
 
 #environmental variable
-export forscr1="environmental variable"
-echo $forscr1
+export forscr2="environmental variable"
+echo $forscr2
 
 
 
 # "FOR CHECK environmental variable"
 export | grep 'declare -x forscr1="environmental variable"'
+
+exit 0
